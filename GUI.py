6af618e06b7c30b2afe8374f4388034875489de4
javiercmh <javiercmh@gmail.com>
2016-01-15 13:12:32 -0300

    files fetched

--- GUI.py
+++ GUI.py
@@ -1,35 +1,75 @@
 #####
 # For next commit:
-# - Create an unified class for ui
+# - Show at least first image
+# - Develop move_pic function
 ####
 
-
+import glob     # to get all files inside a folder
+import os
 from PySide import QtCore, QtGui, QtUiTools
 
 
-# Load a GUI from a .ui file function
-def load_ui_widget(file_path):
-    loader = QtUiTools.QUiLoader()       # file loader
-    ui_file = QtCore.QFile(file_path)    # file object
-    ui_file.open(QtCore.QFile.ReadOnly)
-    ui = loader.load(ui_file, None)      # load file into ui object
-    ui_file.close()
-    return ui
+class UI:
+    def __init__(self, file_path):
+        self.ui = self.load_ui_widget(file_path)
 
+        # to become folder paths
+        self.source_path = ''
+        self.left_path = ''
+        self.right_path = ''
 
-# Function to initialise ui, connecting all buttons and actions
-def main_window_init(ui):
-    ui.button_source.clicked.connect(get_path)  # button source will react with get_path
+        self.files_in_source = list()  # list with all files inside source folder
 
+        self.initialize()   # connect buttons with actions
 
-def get_path():
-    path = QtGui.QFileDialog.getExistingDirectory()  # select folder dialog
-    print("Path:" + path)   # test. Print path
+    # Load a GUI from a .ui file function
+    @staticmethod
+    def load_ui_widget(file_path):
+        loader = QtUiTools.QUiLoader()       # file loader
+        ui_file = QtCore.QFile(file_path)    # file object
+        ui_file.open(QtCore.QFile.ReadOnly)
+        ui = loader.load(ui_file, None)      # load file into ui object
+        ui_file.close()
+        return ui
+
+    # Function to initialise ui, connecting all buttons and actions
+    def initialize(self):
+        self.ui.button_source.clicked.connect(lambda: self.button_press('source'))  # button source will with get_path
+        self.ui.button_left.clicked.connect(lambda: self.button_press('left'))
+        self.ui.button_right.clicked.connect(lambda: self.button_press('right'))
+
+    def button_press(self, sender):
+        # source will always modify source folder path
+        if sender == 'source':
+            self.source_path = QtGui.QFileDialog.getExistingDirectory()  # select folder dialog
+            os.chdir(self.source_path)      # go to source folder
+            self.files_in_source = glob.glob("*.jpg")   # get all files inside source folder
+
+            for file in self.files_in_source:
+                print(os.path.join(self.source_path, file))
+
+        # left button case:
+        elif sender == 'left':
+            if self.left_path == '':    # if left_path is empty, then assign a path to it
+                self.left_path = QtGui.QFileDialog.getExistingDirectory()
+                print("left path is:", self.left_path)
+            else:   # if it isn't, move or copy picture to it
+                self.move_pic('left')
+
+        # right button case:
+        elif sender == 'right':
+            if self.right_path == '':    # if right_path is empty, then assign a path to it
+                self.right_path = QtGui.QFileDialog.getExistingDirectory()
+                print("right path is:", self.right_path)
+            else:   # if it isn't, move or copy picture to it
+                self.move_pic('right')
+
+    def move_pic(self, direction):
+        print("Moving current picture to the", direction)   # pretend something's happening
 
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)  # create application
-    main_window = load_ui_widget("main_window.ui")  # call our function to load a UI
-    main_window_init(main_window)   # initialise ui
-    main_window.show()
+    main_window = UI("main_window.ui")  # define main_window as the UI
+    main_window.ui.show()
     sys.exit(app.exec_())
